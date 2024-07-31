36cb3c31f0f1ceb5e781020f50d32841117bbe95
AndresMorocho <109705281+KAndresMR@users.noreply.github.com>
2024-07-30 22:58:39 -0500

    Conexion con la BD postesgress

---
+++ src/domain/usuarios.ts
@@ -0,0 +1,12 @@
+export interface Usuario {
+    apellido:  string;
+    celular:   string;
+    correo:    string;
+    foto:      string;
+    isAdmin:   boolean;
+    nombre:    string;
+    password:  string;
+    socialUrl: string;
+    ubicacion: string;
+    user:      string;
+}
